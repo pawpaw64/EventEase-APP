9e2e3ebaa38b93f1a9a2014ac630e74ec689b438
pawpaw64 <129535695+pawpaw64@users.noreply.github.com>
2025-02-13 16:46:22 +0600

    Create EventModel.cs

---
+++ Data/EventModel.cs
@@ -0,0 +1,9 @@
+namespace EventEaseApp.Data
+{
+    public class EventModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+    }
+}
